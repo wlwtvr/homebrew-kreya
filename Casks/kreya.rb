2b3b2321ed7f4f24204cba2ebfa648d3069ab224
A Dzulfikar SR <ahmaddzulfikarsr@gmail.com>
2023-09-05 00:05:50 +0700

    feat: supress checksum and version
    remove livecheck
    
    reason: Kreya does not mention version number in download url

--- Casks/kreya.rb
+++ Casks/kreya.rb
@@ -1,21 +1,12 @@
 cask "kreya" do
     arch arm: "arm64", intel: "x64"
-    version "1.11.1"
-    sha256 intel: "3114340fbae77d28bb771e9e5b0f23c2f101c516fa4cc290bde2ff6f41722695",
-            arm: "4b42ea43a9fdb34035cd5138d759fc86eceb48a76aabe9a70d087e0ce94e30ad"
+    version :latest
+    sha256 :nocheck
 
     url "https://stable-downloads.kreya.app/Kreya-osx-#{arch}.zip"
     name "Kreya"
     desc "GUI Client for interacting with gRPC services"
     homepage "https://kreya.app/"
-
-    livecheck do
-        url "https://stable-downloads.kreya.app/Kreya-osx-#{arch}-zip-checksum-sha256.txt"
-        regex(/(\b[0-9a-f]{64}\b)/)
-        strategy :page_match
-    end
-
-    auto_updates true
 
     app "Kreya.app"
 
